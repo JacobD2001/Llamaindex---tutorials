15814276c48c17df3a017818e31e84bd65691a2a
JacobD2001 <dzikowskijakub2001@gmail.com>
2024-11-06 18:39:21 +0100

    chunking

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 from llama_index.core import SimpleDirectoryReader
+from llama_index.core.node_parser import TokenTextSplitter
+from llama_index.core.ingestion import IngestionPipeline
 
-documnets = SimpleDirectoryReader('./data').load_data()
+documents = SimpleDirectoryReader('./data').load_data()
 
 print("loading documents done.")
 
+# Set up the text splitter transformation
+text_splitter = TokenTextSplitter(chunk_size=512, chunk_overlap=10)
+
+# Set up the ingestion pipeline with the splitter
+pipeline = IngestionPipeline(transformations=[text_splitter])
+
+# Run the pipeline to split the documents
+nodes = pipeline.run(documents=documents)
+print("Document splitting done.")
